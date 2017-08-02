56dc675caa7c5d52ff98dbc297ff5c690ff35e8a
lyj <1555322@qq.com>
2017-08-02 10:23:47 +0800

    Change Hello.java In Master Branch

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
 public class Hello {
 	public static void main(String args[]) {
-		System.out.println("Hello GIT");
-		System.out.println("Good!");
-		System.out.println("nihao");
+		Runtime run = Runtime.getRuntime();
+		run.gc();
 	}
 }
