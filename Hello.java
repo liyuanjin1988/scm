1f99745a0ebaae1b78cb9bf93888881291fa9eaf
lyj <1555322@qq.com>
2017-08-02 10:13:16 +0800

    Change Hello.java And Add Admin.java

--- Hello.java
+++ Hello.java
@@ -3,5 +3,7 @@ public class Hello {
 		System.out.println("Hello GIT");
 		System.out.println("Good!");
 		System.out.println("nihao");
+		System.out.println("极限IT：www.jixianit.com");
+		System.out.println("世界和平,没有战争！")
 	}
 }
