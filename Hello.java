3e9109328e6e3671d23a5c309223f7c397e0828b
lyj <1555322@qq.com>
2017-08-09 11:07:18 +0800

    Change Hello.java

--- Hello.java
+++ Hello.java
@@ -7,12 +7,7 @@ public class Hello {
 		System.out.println("Hello GIT");
 		System.out.println("Good!");
 		System.out.println("nihao");
-		System.out.println("极限IT：www.jixianit.com");
-		System.out.println("世界和平,没有战争！");
-		System.out.println("世界和平,没有战争！");
-		System.out.println("世界和平,没有战争！");
-		System.out.println("世界和平,没有战争！");
-		System.out.println("sdfasdfasd");
-		System.out.println("世界和平,没有战争！");
+		System.out.println("www.mldnjava.cn");
+		System.out.println("www.jixianit.com");
 	}
 }
